cb03d7e89206e6b67d8cc53c26248c1097a9a0f6
Edison Simondet <edison.simondet@gmail.com>
2022-01-25 18:24:50 -0600

    no changes, resolved merge conflict by accepting main base

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,18 +1,59 @@
 const router = require('express').Router();
 
-const { User, Pet } = require('../../models');
+const { User, Pet, Post, Comment, Vote } = require('../../models');
 
 // get all users
 
 router.get('/', (req, res) => {
-    User.findAll({
-            attributes: { exclude: ['password'] }
-        })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+  User.findAll({
+    attributes: { exclude: ['password'] },
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Pet,
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  //expects {"email": "email@petsnack.com", "username": "pettest", "password": "password1234"}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//work on login
 
 module.exports = router;
